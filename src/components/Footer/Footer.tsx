54fe2e2137b24e66c7ec8787c14453cdd667438c
PolinaRomanchuk <polinar_99@mail.ru>
2025-06-28 12:30:05 +0300

    style: make new main page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,65 @@
 import type { ReactElement } from 'react';
+import './footer.scss';
+
+import { ReactComponent as Git } from './../../assets/Footer/github.svg';
+import { ReactComponent as Mail } from './../../assets/Footer/circle-envelope.svg';
+import { ReactComponent as Linkedin } from './../../assets/Footer/linkedin.svg';
+import { ReactComponent as Rss } from './../../assets/Footer/rss-logo.svg';
+import Logo from '../../assets/Header/logo.png';
 
 export const Footer = (): ReactElement => {
   return (
     <div className='footer'>
       <div className='_container'>
-        <div className='footer-message'>FloralMuse ©2025. All rights reserved</div>
+        <div className='footer_content'>
+          <div className='sections-container'>
+            <section className='footer_contacts'>
+              <div className='footer_contacts_header'>
+                <h3>Contact</h3>
+              </div>
+              <p className='extra-light'>
+                Made by Polina Romanchuk. To contact me, use the following links
+              </p>
+              <div className='icons-container'>
+                <Git className='footer-icon' />
+                <Mail className='footer-icon' />
+                <Linkedin className='footer-icon' />
+              </div>
+            </section>
+            <section className='footer_about'>
+              <div className='footer_about_header'>
+                <div className='footer_logo-container'>
+                  <img src={Logo} alt='logo' />
+                </div>
+                <h3>FloralMuse</h3>
+              </div>
+              <p className='extra-light'>
+                RSSchool learning project. Online flower & plant store.Built
+                with HTML, CSS, TypeScript, React & CommerceTools
+              </p>
+              <div className='icons-container'>
+                <Rss />
+              </div>
+            </section>
+            <section className='footer_pages'>
+              <div className='footer_pages_header'>
+                <h3>Pages</h3>
+              </div>
+              <div className='pages_container'>
+                <p className='extra-light'>home</p>
+                <p className='extra-light'>shop</p>
+                <p className='extra-light'>about</p>
+                <p className='extra-light'>cart</p>
+                <p className='extra-light'>user</p>
+                <p className='extra-light'>registration</p>
+                <p className='extra-light'>login</p>
+              </div>
+            </section>
+          </div>
+          <div className='under-text'>
+            <p className='regular'>©Learning project 2025 FloralMuse</p>{' '}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,54 +2,208 @@ import { type ReactElement } from 'react';
 import './main.scss';
 import Header, { type HeaderProps } from '../Header/Header';
 import Footer from '../Footer/Footer';
+import { useNavigate } from 'react-router-dom';
+import { ReactComponent as Branch } from './../../assets/Main/branch.svg';
+import { ReactComponent as BlackBranch } from './../../assets/Main/branch2.svg';
+import { ReactComponent as WhiteBranch } from './../../assets/Main/branch3.svg';
+import { ReactComponent as BrownBranch } from './../../assets/Main/branch6.svg';
+import Plant from '../../assets/Main/plant 1.png';
+import Pionies from '../../assets/Main/pion.jpg';
 
-import Promo from '../../assets/promo.png';
-import Promo2 from '../../assets/promo2.png';
-import Promo3 from '../../assets/promo3.png';
-import Promo4 from '../../assets/promo4.png';
+import flowerImg from '../../assets/Main/flower.png';
+const flowers = Array(5).fill({ name: 'Spring flower', image: flowerImg });
 
 export type MainPageProps = HeaderProps & {};
+type ProductCardProps = {
+  price: string;
+  image: string;
+};
 
 export const MainPage = ({ size }: MainPageProps): ReactElement => {
+  const navigate = useNavigate();
+
+  const products = [
+    { id: 1, price: '$180.15', image: Plant },
+    { id: 2, price: '$180.10', image: Plant },
+    { id: 3, price: '$180.15', image: Plant },
+    { id: 4, price: '$90.00', image: Plant },
+    { id: 5, price: '$180.15', image: Plant },
+    { id: 6, price: '$180.15', image: Plant },
+    { id: 7, price: '$180.10', image: Plant },
+    { id: 8, price: '$180.15', image: Plant },
+  ];
+
+  function ProductCard({ price, image }: ProductCardProps): ReactElement {
+    return (
+      <div className='product-card'>
+        <div className='product-img_container'>
+          <img src={image} alt='Ceramic Plant' />
+        </div>
+        <p className='medium'>Ceramic Plant</p>
+        <span className='extra-light'>{price}</span>
+      </div>
+    );
+  }
+
   return (
     <>
       <Header size={size} />
       <main className='main'>
         <div className='_container'>
-          <h2 className='header-main-page'>Active promo codes</h2>
-          <div className='promo-container-list'>
-            <div className='promo-img-container'>
-              <img src={Promo} alt='promo-summer2025'></img>
-              <div className='promo-text-container'>
-                <span className='promo-text'>
-                  Have time to use SUMMER2025 before 31 August 2025
-                </span>
+          <section className='main_welcome-info'>
+            <div className='main_welcome_content'>
+              <div className='main_welcome_header_container'>
+                <h1 className='main_welcome_header'>
+                  The best stories start with flowers
+                </h1>
+              </div>
+              <div className='main_welcome_customers-experience'>
+                <div className='flowers'>
+                  <p className='main_welcome_customers-experience_counter light'>
+                    50+
+                  </p>
+                  <p className='main_welcome_customers-experience_text medium'>
+                    Flowers
+                  </p>
+                </div>
+                <div className='separator'></div>
+                <div className='customers'>
+                  <p className='main_welcome_customers-experience_counter light'>
+                    100+
+                  </p>
+                  <p className='main_welcome_customers-experience_text medium'>
+                    Customers
+                  </p>
+                </div>
+              </div>
+              <div className='main_welcome_button_container'>
+                <button
+                  className='main_welcome_button'
+                  onClick={() => {
+                    navigate('/catalog');
+                  }}
+                >
+                  <span className='main_welcome_button_span'>Shop now</span>
+                </button>
               </div>
             </div>
+          </section>
+          <section className='main_about-us'>
+            <div className='main_about-us-content'>
+              <div className='main_about-us_header-container'>
+                <div className='main_about-us_under-header'>
+                  {' '}
+                  <p className='extra-light'>Who we are</p>
+                </div>
+                <div className='main_about-us_header'>
+                  <h2>Flower’s Wonders Expedition</h2>
+                </div>
+                <div className='img-container'>
+                  <Branch className='main_about-us_branch' />
+                </div>
+              </div>
+              <div className='main_about-us_text_container'>
+                <div className='main_about-us_text'>
+                  <p className='medium'>
+                    This website was created as an educational project for the
+                    <a
+                      href='https://rs.school/'
+                      target='_blank'
+                      className='rss_link'
+                    >
+                      {' '}
+                      RSSchool
+                    </a>{' '}
+                    courses.
+                  </p>
+                  <p className='extra-light main_about-us_p'>
+                    It serves as a demonstration of web development techniques
+                    and is not a commercial product. Built with HTML, CSS,
+                    TypeScript, React, and CommerceTools, it showcases practical
+                    implementation of frontend development and e-commerce
+                    integration
+                  </p>
+                  <p className='extra-light main_about-us_p'>
+                    Thank you for visiting!
+                  </p>
+                </div>
+                <div className='main_about-us_button'>
+                  <button>
+                    <span>About us</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className='main_saled_plants'>
+            <div className='main_saled_plants_content'>
+              <div className='main_saled_plants_header_container'>
+                <p className='extra-light'>New Collection</p>
+                <div className='branch_container'>
+                  <BlackBranch />
+                </div>
+                <h2>House Creative Plant</h2>
+              </div>
+              <div className='grid-container'>
+                {products.slice(0, 3).map((product) => (
+                  <ProductCard key={product.id} {...product} />
+                ))}
 
-            <div className='promo-img-container'>
-              <img src={Promo2} alt='promo-fallsave'></img>
-              <div className='promo-text-container'>
-                <span className='promo-text'>
-                  The FALLSAVE gives you a 15% discount
-                </span>
+                <div className='featured'>
+                  <img src={Pionies} alt='Featured Plant' />
+                  <div className='pionies-label'>
+                    <p className='extra-light'>We also have peonies</p>
+                    <div className='pionies_button-container'>
+                      <button>
+                        <span>view more</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='right-column'>
+                  {products.slice(3, 4).map((product) => (
+                    <ProductCard key={product.id} {...product} />
+                  ))}
+                </div>
+
+                {products.slice(4).map((product) => (
+                  <ProductCard key={product.id} {...product} />
+                ))}
               </div>
             </div>
-            <div className='promo-img-container'>
-              <img src={Promo4} alt='promo-sale20'></img>
-              <div className='promo-text-container'>
-                <span className='promo-text'>Get the best 20% discount</span>
+          </section>
+          <section className='main_promo'>
+            <div className='main_promo_content'>
+              <div className='main_promo_header_container'>
+                <p className='extra-light'>Promo code</p>
+                <WhiteBranch />
+              </div>
+              <div className='main_promo_text'>
+                <h3>
+                  Use the promo code HAPPY to get a 10% discount on houseplants.
+                </h3>
               </div>
             </div>
-            <div className='promo-img-container'>
-              <img src={Promo3} alt='promo-happy'></img>
-              <div className='promo-text-container'>
-                <span className='promo-text'>
-                  To be happy use HAPPY and get 5% discount
-                </span>
+          </section>
+          <section className='main_flowers-category'>
+            <div className='main_flowers-category_content'>
+
+              <div className='flower-grid'>
+              <div className='left-text'>
+                <p className='extra-light'>Discover category</p>
+                <h2>All kind of Flowers</h2>
+                <BrownBranch />
               </div>
+
+              {flowers.map((flower, index) => (
+                <div className='flower-card' key={index}>
+                  <img src={flower.image} alt={flower.name} />
+                  <p className='regular'>{flower.name}</p>
+                </div>
+              ))}</div>
             </div>
-          </div>
+          </section>
         </div>
       </main>
       <Footer />
